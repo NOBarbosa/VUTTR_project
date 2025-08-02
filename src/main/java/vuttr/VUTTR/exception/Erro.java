5ba907e776695472d8512e5950e113c60e32dd52
Natalia Oliveira <nob.ufba@gmail.com>
2025-08-02 11:46:48 -0300

    feat: Exception handler

--- src/main/java/vuttr/VUTTR/exception/Erro.java
+++ src/main/java/vuttr/VUTTR/exception/Erro.java
@@ -1,4 +1,27 @@
 package vuttr.VUTTR.exception;
 
 public class Erro {
+ public String message;
+ public int status;
+
+    public Erro(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
--- src/main/java/vuttr/VUTTR/exception/GlobalExceptionHandler.java
+++ src/main/java/vuttr/VUTTR/exception/GlobalExceptionHandler.java
@@ -1,4 +1,28 @@
 package vuttr.VUTTR.exception;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<Erro> handleGenericException(Exception e){
+        String message = e.getMessage();
+        if(e instanceof MethodArgumentTypeMismatchException){
+            message = "Tipo inválido";
+        }
+        Erro erro = new Erro(message, 400);
+        e.printStackTrace();
+        return ResponseEntity.badRequest().body(erro);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Erro> handleNotFoundException(NotFoundException e){
+        Erro erro =  new Erro(e.getMessage(), 404);
+        return ResponseEntity.badRequest().body(erro);
+
+    }
 }
